d9b94130a00465222b017f619c98c78b0cdccd70
Bebek_hub <godsfavourtolu@gmail.com>
2024-06-02 23:46:10 +0100

    adding API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://restcountries.com';
+const apiURL = 'https://restcountries.com/v3.1/all';
 
 fetch(apiURL)
   .then(response => {
@@ -13,3 +13,19 @@ fetch(apiURL)
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
   });
+
+  function displayData(data) {
+    const container = document.getElementById('data-container');
+  
+    data.forEach(item => {
+      const div = document.createElement('div');
+      div.className = 'data-item';
+      div.innerHTML = `
+        <h3>${item.name/{name}}</h3>
+        <p>${item.name/population}</p>
+        <p>${item.region}</p>
+        <p>${item.capital}</p>
+      `;
+      container.appendChild(div);
+    });
+  }
