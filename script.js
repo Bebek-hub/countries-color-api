d3457a811e478900622abefee8f4c10953946d76
Bebek_hub <godsfavourtolu@gmail.com>
2024-06-02 22:50:58 +0100

    adding js

---
+++ script.js
@@ -0,0 +1,15 @@
+const apiURL = 'https://restcountries.com';
+
+fetch(apiURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
