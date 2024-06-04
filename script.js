bad367bb0cb2d99880395754c5b26ebfff2700a0
Bebek_hub <godsfavourtolu@gmail.com>
2024-06-04 21:25:24 +0100

    adding more functionalities

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-const apiURL = 'https://restcountries.com/v3.1/all';
+
+
+
+const apiURL = "https://restcountries.com/v3.1/all";
 
 fetch(apiURL)
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+      throw new Error("Network response was not ok " + response.statusText);
     }
     return response.json();
   })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+  .then((data) => {
+    const container = document.getElementById("data-container");
 
-  function displayData(data) {
-    const container = document.getElementById('data-container');
-  
-    data.forEach(item => {
-      const div = document.createElement('div');
-      div.className = 'data-item';
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.className = "data-item";
       div.innerHTML = `
-        <h3>${item.name/{name}}</h3>
-        <p>${item.name/population}</p>
-        <p>${item.region}</p>
-        <p>${item.capital}</p>
+      <img src="${item.flags.png}" alt="${item.flags.png}" />
+        <h3>${item.name.common}</h3>
+        <p>Population: ${item.population}</p>
+        <p>Region: ${item.continents}</p>
+        <p>Capital: ${item.capital}</p>
       `;
       container.appendChild(div);
     });
-  }
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
+
+
